d951ee4b18c7be89ae05d00c0d9b69cb6d214ef2
Pedro Rosa <devpedrorosa@gmail.com>
2023-11-30 18:14:40 -0300

    feat(home): homepage with degrade

--- assets/js/modeDarkLight.js
+++ assets/js/modeDarkLight.js
@@ -2,11 +2,13 @@ let rail = document.getElementById('rail');
 let body = document.querySelector('body');
 let textLogo = document.querySelector('.text-logo');
 let navLinks = document.querySelectorAll('.nav-link');
+let navBar = document.querySelector('.nav-bar');
 
 rail.addEventListener('click', () => {
   rail.classList.toggle('dark');
   body.classList.toggle('dark');
   textLogo.classList.toggle('dark');
+  navBar.classList.toggle('dark');
 
   navLinks.forEach((navLink) => {
     navLink.classList.toggle('dark');
