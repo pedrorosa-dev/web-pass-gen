e08f4125b79c75ccd786ed97f8da3530b1f66f72
Izabelle Luna <personalife503@gmail.com>
2023-12-18 13:10:27 -0300

    improvements added to navbar hamburguer and style

--- assets/styles/mobile/mobile-nav-bar.js
+++ assets/styles/mobile/mobile-nav-bar.js
@@ -1,19 +1,19 @@
-const navButton = document.querySelector('#hamburger-toggle');
+const navButton = document.querySelector("#hamburger-toggle");
 
-navButton.addEventListener('click', (event) => {
+navButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const navList = document.querySelector('.nav-list');
-  navList.style.display = navList.style.display === 'block' ? 'none' : 'block';
+  const navList = document.querySelector(".nav-list");
+  navList.style.display = navList.style.display === "block" ? "none" : "block";
 });
 
-const mql = window.matchMedia('(min-width: 768px)');
-mql.addEventListener('change', (event) => {
-  const navList = document.querySelector('.nav-list');
+const mql = window.matchMedia("(min-width: 768px)");
+mql.addEventListener("change", (event) => {
+  const navList = document.querySelector(".nav-list");
 
   if (event.matches) {
-    navList.style.display = 'flex';
+    navList.style.display = "flex";
   } else {
-    navList.style.display = 'none';
+    navList.style.display = "none";
   }
 });
